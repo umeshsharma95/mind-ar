edef5417e06b7435bb7c2e9ed4d05766d0dbed92
Umesh Sharma <umeshs@iprogrammer.com>
2023-11-22 12:21:33 +0530

    santa mask

--- index.js
+++ index.js
@@ -5,28 +5,39 @@ document.addEventListener("DOMContentLoaded", function() {
 	  arSystem = sceneEl.systems["mindar-image-system"];
 	});
   const nextButtonWrapper = document.querySelector('.nextButtonWrapper');
-	
+	const scanningOverlay = document.querySelector('#example-scanning-overlay');
+
   [0, 1, 2, 3, 4].forEach((_, index) => {
     const imageTarget = document.getElementById(
       `mindar-image-target_${index}`
     );
     imageTarget.addEventListener('targetFound', (event) => {
       console.log('targetFound', index);
+      loadAnimation()
       nextButtonWrapper.style.display = 'flex'
+      scanningOverlay.style.display = 'none'
       const nextButton = document.querySelector('#nextButton');
       nextButton.addEventListener('click', () => {
         console.log('nextButton click');
-        window.location.replace("./main.html")
+        window.location.reload()
       })
     });
     imageTarget.addEventListener('targetLost', (event) => {
       console.log('target lost');
-      nextButtonWrapper.style.display = 'none'
     });
   });
 
 });
 
+const loadAnimation = () => {
+  var svgContainer = document.getElementById('svgContainer');
+  var animItem = bodymovin.loadAnimation({
+    wrapper: svgContainer,
+    animType: 'svg',
+    loop: true,
+    path: './animation.json'
+  });
+}
 
 
 
--- main.js
+++ main.js
@@ -1,64 +1,57 @@
+// function saveBase64AsFile(base64, fileName='download') {
+//     var link = document.createElement("a");
+//     document.body.appendChild(link); // for Firefox
+//     link.setAttribute("href", base64);
+//     link.setAttribute("download", fileName);
+//     link.click();
+// }
 
+// function takeScreenshot() {
+// 	var screenshot = document.documentElement
+// 		.cloneNode(true);
+// 	screenshot.style.pointerEvents = 'none';
+// 	screenshot.style.overflow = 'hidden';
+// 	screenshot.style.webkitUserSelect = 'none';
+// 	screenshot.style.mozUserSelect = 'none';
+// 	screenshot.style.msUserSelect = 'none';
+// 	screenshot.style.oUserSelect = 'none';
+// 	screenshot.style.userSelect = 'none';
+// 	screenshot.dataset.scrollX = window.scrollX;
+// 	screenshot.dataset.scrollY = window.scrollY;
+// 	var blob = new Blob([screenshot.outerHTML], {
+// 		type: 'text/html'
+// 	});
+// 	return blob;
+// }
 
-function take_snapshot() {
-    const screenshotTarget = document.body;
-    html2canvas(screenshotTarget).then(canvas => {
-        // to image as png use below line
-        // const base64image = canvas.toDataURL("image/png");
-        // show the image in window use below line
-        // window.location.href = base64image;
-        
-        // screenshot appended to the body as canvas
-        document.body.appendChild(canvas);  
-        dataURL = canvas.toDataURL();  
-        // to print the screenshot in console use below line
-        // console.log(dataURL);
-        
-        // following line is optional and it is to save the screenshot
-        // on the server side. It initiates an ajax call
-        saveBase64AsFile(dataURL); 
-    });  
-}
+// const container = document.getElementById("container");
+// const captureButton = document.getElementById("capture-button");
+// const previewContainer = document.getElementById("preview-container");
+// const downloadButton = document.getElementById("download-button");
 
-function saveBase64AsFile(base64, fileName='download') {
-    var link = document.createElement("a");
-    document.body.appendChild(link); // for Firefox
-    link.setAttribute("href", base64);
-    link.setAttribute("download", fileName);
-    link.click();
-}
+// captureButton.addEventListener("click", async () => {
+//   const canvas = await html2canvas(container);
+//   const imageURL = canvas.toDataURL();
+//   saveBase64AsFile(imageURL)
+// });
 
-function takeScreenshot() {
-	var screenshot = document.documentElement
-		.cloneNode(true);
-	screenshot.style.pointerEvents = 'none';
-	screenshot.style.overflow = 'hidden';
-	screenshot.style.webkitUserSelect = 'none';
-	screenshot.style.mozUserSelect = 'none';
-	screenshot.style.msUserSelect = 'none';
-	screenshot.style.oUserSelect = 'none';
-	screenshot.style.userSelect = 'none';
-	screenshot.dataset.scrollX = window.scrollX;
-	screenshot.dataset.scrollY = window.scrollY;
-	var blob = new Blob([screenshot.outerHTML], {
-		type: 'text/html'
+const loadAnimation = () => {
+	var christmasFall = document.getElementById('christmas_fall');
+	var christmasVan = document.getElementById('christmas_van');
+	// var animItem = bodymovin.loadAnimation({
+	//   container: christmasFall,
+	//   renderer: 'svg',
+	//   loop: true,
+	//   path: './christmas_fall.json'
+	// });
+	var animItem = bodymovin.loadAnimation({
+		container: christmasVan,
+		renderer: 'svg',
+		loop: true,
+		path: './christmas_van.json'
 	});
-	return blob;
-}
+  }
 
-function generate() {
-	window.URL = window.URL || window.webkitURL;
-	window.open(window.URL
-		.createObjectURL(takeScreenshot()));
-}
-
-const container = document.getElementById("container");
-const captureButton = document.getElementById("capture-button");
-const previewContainer = document.getElementById("preview-container");
-const downloadButton = document.getElementById("download-button");
-
-captureButton.addEventListener("click", async () => {
-  const canvas = await html2canvas(container);
-  const imageURL = canvas.toDataURL();
-  saveBase64AsFile(imageURL)
-});
+document.addEventListener("DOMContentLoaded", function() {
+	loadAnimation()
+})
